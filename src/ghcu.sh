e7ecc0b86710b939b35c7033a11e9e23b426f4ad
nick <nflower@asteria.com>
2020-09-25 16:46:50 +0900

    testing update

--- src/ghcu.sh
+++ src/ghcu.sh
@@ -161,7 +161,7 @@ update() {
     rm -f ~/ghcu.sh
     wget -P ~/ https://raw.githubusercontent.com/0xNF/ghcu/master/src/ghcu.sh
     chmod +x ~/ghcu.sh
-    echo "Updated to latest version of ghcu"
+    echo "Updated to latest version of ghcu - hello"
 }
 
 bytes2mb() {
